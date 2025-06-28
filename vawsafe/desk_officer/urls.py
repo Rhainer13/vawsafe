1f40290b0977170b7c65252023448ab6b669bdde
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-06-28 11:30:09 +0800

    Minor UI Changes

--- vawsafe/desk_officer/urls.py
+++ vawsafe/desk_officer/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = "desk_officer"
 urlpatterns = [
-    path("", views.index, name="desk_officer-index"),
-    path("victim_list", views.victim_list, name="desk_officer-victim_list")
+    path("", views.index, name="index"),
+    path("victim_list/", views.victim_list, name="victim_list"),
+
+    path("social_worker_list/", views.social_worker_list, name="social_worker_list"),
+    path("add_social_worker/", views.add_social_worker, name="add_social_worker"),
+    path("social_worker_case/", views.social_worker_case, name="social_worker_case"),
 ]
--- vawsafe/desk_officer/views.py
+++ vawsafe/desk_officer/views.py
@@ -7,3 +7,13 @@ def index(request):
 
 def victim_list(request):
     return render(request, 'desk_officer/victim_tab/victim_list.html')
+
+# social worker views
+def social_worker_list(request):
+    return render(request, 'desk_officer/social_worker_tab/list_social_worker.html')
+
+def add_social_worker(request): 
+    return render(request, 'desk_officer/social_worker_tab/add_social_worker.html')
+
+def social_worker_case(request):
+    return render(request, 'desk_officer/social_worker_tab/social_worker_case.html')
